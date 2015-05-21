8781f39b15c3ec9e57b7e8684f319ba7839450bf
TPhilippon <terence.philippon@sfr.fr>
2015-05-21 12:33:58 +0200

    Edit1

--- Meditera/4_animation.py
+++ Meditera/4_animation.py
@@ -4,22 +4,27 @@ matplotlib.use('TkAgg') # do this before importing pylab
 import matplotlib.pyplot as plt
 import Image
 import glob
+import os
 
 varg=['sst11mic_8d','poc_8d', 'nsst_8d','chl_8d']
 varg=varg[1]  # Choisir la variable géochimique
-path = varg+'/png'
+path = '../../../ddata/'+varg+'/png'
+print os.getcwd()
+
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
 def animate():
     filenames=sorted(glob.glob(path+'/*.png'))
+    print filenames
     im=plt.imshow(Image.open(filenames[0]))
     for filename in filenames[1:]:
         image=Image.open(filename)
         image=image.transpose(Image.FLIP_TOP_BOTTOM)
         im.set_data(image)
-        
+       
         fig.canvas.draw() 
 
 win = fig.canvas.manager.window
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Satellital GeoData extraction and time series
 # The purpose is to extract a zone from NASA database or other possible satellital imagery.
-# We wanted to include coordinates choice to allow more treatments, and geographical analysis.
+# We wanted to include coordinate choices to allow more treatments, and geographical analysis.
 
